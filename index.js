9f2e74e89b20ccbe3addafca1b0f1bcc00bdcbb6
dinesh-swami <dinesh907989@gmail.com>
2026-04-09 00:26:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ function decrypt(encryptedPassword) {
 
     return password;
 };
+function decrypt(encryptedPassword) {
+    let password = "";
+    
+    for (let i = 0; i < encryptedPassword.length; i) {
+        password += String.fromCharCode(encryptedPassword[i].charCodeAt(0) - 1);
+        i = i+4;
+    }
+
+    return password;
+};
 console.log(decrypt('bkc2ni04bcKJoLVs!bAkt8F2bCiJmKqjvs0XkUSnbMIj!3JvA1sU/KJzdVTkpXwwna5V!KdK:Ldz1Bqr8BXQ:7TU!Xtp�cGe�Fji!HBVऱSVkौN0UहnYr'))
 wwna5V!KdK:Ldz1Bqr8BXQ:7TU!Xtp�cGe�Fji!HBVऱSVkौN0UहnYr'))
 console.log(decrypt('bkc2ni04bcKJoLVs!bAkt8F2bCiJmKqjvs0XkUSnbMIj!3JvA1sU/KJzdVTkpXwwna5V!KdK:Ldz1Bqr8BXQ:7TU!Xtp�cGe�Fji!HBVऱSVkौN0UहnYr'))
