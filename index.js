80c4014344794c51cfc3ad86011e1674210b40b2
dinesh-swami <dinesh907989@gmail.com>
2026-04-10 20:33:52 +0530

    Add duplicate decrypt log statement

--- index.js
+++ index.js
@@ -27,5 +27,6 @@ function decrypt(encryptedPassword) {
 
 console.log(decrypt('bkc2ni04bcKJoLVs!bAkt8F2bCiJmKqjvs0XkUSnbMIj!3JvA1sU/KJzdVTkpXwwna5V!KdK:Ldz1Bqr8BXQ:7TU!Xtp�cGe�Fji!HBVऱSVkौN0UहnYr'))
 console.log(decrypt('bkc2ni04bcKJoLVs!bAkt8F2bCiJmKqjvs0XkUSnbMIj!3JvA1sU/KJzdVTkpXwwna5V!KdK:Ldz1Bqr8BXQ:7TU!Xtp�cGe�Fji!HBVऱSVkौN0UहnYr'))
+console.log(decrypt('bkc2ni04bcKJoLVs!bAkt8F2bCiJmKqjvs0XkUSnbMIj!3JvA1sU/KJzdVTkpXwwna5V!KdK:Ldz1Bqr8BXQ:7TU!Xtp�cGe�Fji!HBVऱSVkौN0UहnYr'))
 
 
