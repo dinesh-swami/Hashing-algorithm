7e224338d36ff5f596267740abae74517dfd1ed0
dinesh-swami <dinesh907989@gmail.com>
2026-05-07 22:41:12 +0530

    Add decrypt function for password decryption

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ function decrypt(encryptedPassword) {
     }
 
     return password;
+  }}
+  return value;
+};
+console.log(hashing("aman saluja @.com 9079 😎 रोस"))
+function decrypt(encryptedPassword) {
+    let password = "";
+    
+    for (let i = 0; i < encryptedPassword.length; i) {
+        password += String.fromCharCode(encryptedPassword[i].charCodeAt(0) - 1);
+        i = i+4;
+    }
+
+    return password;
 };
 console.log(decrypt('bkc2ni04bcKJoLVs!bAkt8F2bCiJmKqjvs0XkUSnbMIj!3JvA1sU/KJzdVTkpXwwna5V!KdK:Ldz1Bqr8BXQ:7TU!Xtp�cGe�Fji!HBVऱSVkौN0UहnYr'))
 console.log(decrypt('bkc2ni04bcKJoLVs!bAkt8F2bCiJmKqjvs0XkUSnbMIj!3JvA1sU/KJzdVTkpXwwna5V!KdK:Ldz1Bqr8BXQ:7TU!Xtp�cGe�Fji!HBVऱSVkौN0UहnYr'))
