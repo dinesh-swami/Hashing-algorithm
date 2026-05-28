5340668d8457fd50bb72c0ff140b37a2769eb02d
dinesh-swami <dinesh907989@gmail.com>
2026-05-28 14:55:51 +0530

    Update index.jsUpdate index.js

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ function decrypt(encryptedPassword) {
   }}
   return value;
 };
-console.log(hashing("aman saluja @.com 9079 😎  रोस"))
+console.log(hashing("aman saluja @.com 9079 😎 रोस"))
+console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎  रोस"))
 console.log(hashing("aman saluja @.com 9079 😎  रोस"))
 console.log(hashing("aman saluja @.com 9079 😎  रोस"))
