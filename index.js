e49c790b78f1d7f67845db27d19da3f25acf4ac9
dinesh-swami <dinesh907989@gmail.com>
2026-06-05 20:04:38 +0530

    add.

--- index.js
+++ index.js
@@ -35,4 +35,5 @@ console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
+console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 
