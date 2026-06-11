4b3a84b1a5f2d20fe865b6ff3675dae4d211d9d8
dinesh-swami <dinesh907989@gmail.com>
2026-06-11 13:56:39 +0530

    just updated more advanced features in algorigthm.

--- index.js
+++ index.js
@@ -29,20 +29,6 @@ function decrypt(encryptedPassword) {
 };
 };
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079😎 रोस"))
-console.log(hashing("aman saluja @.com 9079😎 रोस"))
-console.log(hashing("aman saluja @.com 9079😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-console.log(hashing("aman saluja @.com 9079 😎 रोस"))
-
 
 
 
