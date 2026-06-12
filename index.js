de5040e6969a14c4f8d18f4e412a41f90c54cedd
dinesh-swami <dinesh907989@gmail.com>
2026-06-12 15:13:28 +0530

    update hashing inrenshing standard.

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
+console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 
 
 
