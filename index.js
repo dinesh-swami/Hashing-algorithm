454c96adcd02822eed7c86fc3b0427efb2af327a
dinesh-swami <dinesh907989@gmail.com>
2026-06-13 10:31:46 +0530

    more upgrade algo with typings

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 console.log(hashing("aman saluja @.com 9079 😎 रोस"))
+console.log(hashing("aman saluja @.com 9079 😎 रोस"))
 
 
 
